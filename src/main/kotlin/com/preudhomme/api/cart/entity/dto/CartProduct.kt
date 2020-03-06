12e1130f1368bb083d860173c544f876f3ec6ee4
mael chauvet <shyndard@gmail.com>
2020-03-06 16:49:02 +0100

    Add vat type

--- src/main/kotlin/com/preudhomme/api/cart/entity/dto/CartProduct.kt
+++ src/main/kotlin/com/preudhomme/api/cart/entity/dto/CartProduct.kt
@@ -2,4 +2,4 @@ package com.preudhomme.api.cart.entity.dto
 
 import java.util.*
 
-data class CartProduct(val id: UUID, val name: String, val price: Float, val amount: Int)
+data class CartProduct(val id: UUID, val name: String, val price: Float, val amount: Int, val vatType: String)
