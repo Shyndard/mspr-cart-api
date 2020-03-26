18934b85e19f05de4d16ddb36e53cbc8c11205e4
Maël <shyndard@gmail.com>
2020-03-26 15:31:03 +0100

    :sparkles: Converting user id from uuid to string (#35)

--- src/main/kotlin/com/preudhomme/api/cart/entity/dto/CartCreation.kt
+++ src/main/kotlin/com/preudhomme/api/cart/entity/dto/CartCreation.kt
@@ -2,4 +2,4 @@ package com.preudhomme.api.cart.entity.dto
 
 import java.util.*
 
-data class CartCreation(val userId: UUID, val products: Array<CartProductCreation>)
+data class CartCreation(val userId: String, val products: Array<CartProductCreation>)
